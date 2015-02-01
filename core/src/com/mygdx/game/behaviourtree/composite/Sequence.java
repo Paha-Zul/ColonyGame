4335c6ef00bf291c8954c0071f8a95ea0f49f29e
Pahasz <Bbenton91@gmail.com>
2015-02-01 18:11:07 -0500

    Removing Composite. Adding Sequence.

---
+++ core/src/com/mygdx/game/behaviourtree/composite/Sequence.java
@@ -0,0 +1,52 @@
+package com.mygdx.game.behaviourtree.composite;
+
+import com.mygdx.game.behaviourtree.ParentTask;
+import com.mygdx.game.behaviourtree.Task;
+import com.mygdx.game.behaviourtree.control.ParentTaskController;
+import com.mygdx.game.component.BlackBoard;
+
+/**
+ * Created by Bbent_000 on 12/31/2014.
+ */
+public class Sequence extends ParentTask {
+
+    public Sequence(String name, BlackBoard blackBoard){
+        super(name, blackBoard);
+    }
+
+    @Override
+    public boolean check() {
+        return control.getSubTasks().size() > 0;
+    }
+
+    @Override
+    public void start() {
+        this.control.currTask = this.control.getSubTasks().get(0);
+    }
+
+    @Override
+    public void update(float delta) {
+        super.update(delta);
+    }
+
+    @Override
+    public void end() {
+
+    }
+
+    @Override
+    public void childSucceeded() {
+        this.control.currIndex++; //Increment the index.
+        //If the index is still less than the size, get the next task.
+        if(this.control.currIndex < this.control.getSubTasks().size())
+            this.control.currTask = this.control.getSubTasks().get(this.control.currIndex);
+        //If we ran out of tasks, finish with success.
+        else
+            this.control.finishWithSuccess();
+    }
+
+    @Override
+    public void childFailed() {
+        this.control.finishWithFailure();
+    }
+}
