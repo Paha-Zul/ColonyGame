16dba5b358b25f9abebd66dcd0b10274efc11525
Pahasz <Bbenton91@gmail.com>
2015-02-19 20:42:15 -0500

    Random chance to be different tree types.

--- core/src/com/mygdx/game/entity/TreeEnt.java
+++ core/src/com/mygdx/game/entity/TreeEnt.java
@@ -2,6 +2,7 @@ package com.mygdx.game.entity;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
@@ -12,6 +13,7 @@ import com.mygdx.game.component.Interactable;
 import com.mygdx.game.component.Resource;
 import com.mygdx.game.component.collider.Collider;
 import com.mygdx.game.helpers.Constants;
+import com.mygdx.game.helpers.managers.ResourceManager;
 
 /**
  * Created by Paha on 1/29/2015.
@@ -22,8 +24,11 @@ public class TreeEnt extends Entity{
 
         this.addTag(Constants.ENTITY_RESOURCE);
 
+        float rand = MathUtils.random();
+        if(rand < 0.2) this.addComponent(ResourceManager.getResourceByname("redwood"));
+        else this.addComponent(ResourceManager.getResourceByname("pine"));
+
         this.addComponent(new Interactable("resource"));
-        this.addComponent(new Resource("Wood Log"));
         this.addComponent(new GridComponent(Constants.GRIDSTATIC, ColonyGame.worldGrid, -1));
 
         this.makeCollider();
