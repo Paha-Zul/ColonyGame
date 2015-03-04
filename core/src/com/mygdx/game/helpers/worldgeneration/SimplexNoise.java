6593ebf76f02f4ba7c615fac2b52c84e517734ab
Pahasz <Bbenton91@gmail.com>
2015-03-03 21:18:14 -0500

    Reverted back to a static class.

--- core/src/com/mygdx/game/helpers/worldgeneration/SimplexNoise.java
+++ core/src/com/mygdx/game/helpers/worldgeneration/SimplexNoise.java
@@ -7,11 +7,11 @@ import java.util.Random;
  */
 public class SimplexNoise {
 
-    private int grad3[][] = { {1,1,0},{-1,1,0},{1,-1,0},{-1,-1,0},
+    private static int grad3[][] = { {1,1,0},{-1,1,0},{1,-1,0},{-1,-1,0},
             {1,0,1},{-1,0,1},{1,0,-1},{-1,0,-1},
             {0,1,1},{0,-1,1},{0,1,-1},{0,-1,-1}};
 
-    private int p[] = { 151,160,137,91,90,15,
+    private static int p[] = { 151,160,137,91,90,15,
             131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
             190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
             88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
@@ -26,30 +26,23 @@ public class SimplexNoise {
             138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180};
 
     // To remove the need for index wrapping, double the permutation table length
-    private int perm[] = new int[512];
-
-    /**
-     * Generates SimplexNoise with a seed.
-     * @param seed
-     */
-    public SimplexNoise(long seed){
+    private static int perm[] = new int[512];
+    static {
         for(int i = 0; i < 512; i++)
             perm[i] = p[i & 255];
-
-        genGrad(seed);
     }
 
     // This method is a *lot* faster than using (int)Math.floor(x)
-    private int fastfloor(double x) {
+    private static int fastfloor(double x) {
         return x > 0 ? (int)x : (int)x - 1;
     }
 
-    private double dot(int g[], double x, double y) {
+    private static double dot(int g[], double x, double y) {
         return g[0] * x + g[1] * y;
     }
 
     // 2D simplex noise
-    public double noise(double xin, double yin) {
+    public static double noise(double xin, double yin) {
         double n0, n1, n2;
 
         final double F2 = 0.5 * (Math.sqrt(3.0) - 1.0);
@@ -111,7 +104,7 @@ public class SimplexNoise {
         return 70.0 * (n0 + n1 + n2);
     }
 
-    public void genGrad(long seed) {
+    public static void genGrad(long seed) {
         Random rnd = new Random(seed);
         for(int i = 0; i < 255; i++)
             p[i] = i;
