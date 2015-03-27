0b7ecb296235dd4dc5a9f304c37ac65f01dc8c8b
Pahasz <Bbenton91@gmail.com>
2015-03-27 05:06:38 -0400

    Added Collision class which implements the ContactListener.

---
+++ core/src/com/mygdx/game/helpers/Collision.java
@@ -0,0 +1,40 @@
+package com.mygdx.game.helpers;
+
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+import com.mygdx.game.entity.Entity;
+
+/**
+ * Created by Paha on 3/26/2015.
+ */
+public class Collision implements ContactListener {
+
+    @Override
+    public void beginContact(Contact contact) {
+        Entity first = (Entity)contact.getFixtureA().getBody().getUserData();
+        Entity other = (Entity)contact.getFixtureB().getBody().getUserData();
+
+        EventSystem.notify(first, "collidestart", other);
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+        Entity first = (Entity)contact.getFixtureA().getBody().getUserData();
+        Entity other = (Entity)contact.getFixtureB().getBody().getUserData();
+
+        EventSystem.notify(first, "collideend", other);
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+
+    }
+}
