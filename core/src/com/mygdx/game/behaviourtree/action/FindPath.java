0cfe36199c8a2e390cd663930839dd08eda44454
Pahasz <Bbenton91@gmail.com>
2015-07-20 17:12:14 -0400

    Working on stuff entering buildings. Also moved the Pathfinding logic to a separate class instead of inside a task.

--- core/src/com/mygdx/game/behaviourtree/action/FindPath.java
+++ core/src/com/mygdx/game/behaviourtree/action/FindPath.java
@@ -1,16 +1,10 @@
 package com.mygdx.game.behaviourtree.action;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.ColonyGame;
 import com.mygdx.game.behaviourtree.LeafTask;
 import com.mygdx.game.util.BlackBoard;
-import com.mygdx.game.util.GH;
 import com.mygdx.game.util.Grid;
-
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
+import com.mygdx.game.util.Pathfinder;
 
 /**
  * A Task that finds a path from where the Entity owner of this task is standing to a target Entity or target node.
@@ -34,7 +28,14 @@ public class FindPath extends LeafTask {
         if(this.blackBoard.target != null)
             this.blackBoard.targetNode = ColonyGame.worldGrid.getNode(this.blackBoard.target);
 
-        this.blackBoard.path = getPath();
+        Grid.Node startNode = this.blackBoard.colonyGrid.getNode(this.blackBoard.myManager.getEntityOwner());
+        Grid.Node targetNode;
+        if(this.blackBoard.targetNode != null)
+            targetNode = this.blackBoard.targetNode;
+        else
+            targetNode = this.blackBoard.colonyGrid.getNode(this.blackBoard.target);
+
+        this.blackBoard.path = Pathfinder.findPath(startNode, targetNode);
         this.control.finishWithSuccess();
     }
 
@@ -46,105 +47,5 @@ public class FindPath extends LeafTask {
     @Override
     public void end() {
         super.end();
-    }
-
-    public LinkedList<Vector2> getPath(){
-        boolean found = false;
-        Grid.PathNode target = null;
-        Grid.Node targetNode;
-
-        //If we have a target node in the blackboard, use it.
-        if(this.blackBoard.targetNode != null)
-            targetNode = this.blackBoard.targetNode;
-
-        //Otherwise, if we have an Entity target, get the node from that.
-        else if(this.blackBoard.target != null)
-            targetNode = this.blackBoard.colonyGrid.getNode(this.blackBoard.target);
-
-        //Otherwise, end with failure.
-        else{
-            this.control.finishWithFailure();
-            return new LinkedList<>();
-        }
-
-        //If our targetNode is still null somehow, fail and return.
-        if(targetNode == null) {
-            Gdx.app.log("[FindPath]:",this.blackBoard.myManager.getEntityOwner().name + "'s target is null");
-            this.control.finishWithFailure();
-            return null;
-        }
-
-        LinkedList<Vector2> path = new LinkedList<>(); //Will hold the end path.
-        HashMap<Grid.Node, Grid.Node> visitedMap = new HashMap<>();
-
-        //Priority queue that sorts by the lowest F value of the PathNode.
-        PriorityQueue<Grid.PathNode> openList = new PriorityQueue<>((n1, n2) -> (int)((n1.getF() - n2.getF())*100));
-
-        //Set the starting currNode and its G and H value.
-        Grid.Node node = this.blackBoard.colonyGrid.getNode(this.blackBoard.myManager.getEntityOwner());
-        int[] index = this.blackBoard.colonyGrid.getIndex(this.blackBoard.myManager.getEntityOwner().getTransform().getPosition());
-        if(node == null) GH.writeErrorMessage("Node was null for entity: "+this.blackBoard.myManager.getEntityOwner().name+" at "+ this.blackBoard.myManager.getEntityOwner().getTransform().getPosition()
-                +" which is index "+index[0]+" "+index[1]+" when grid is size "+this.blackBoard.colonyGrid.getWidth()+" "+this.blackBoard.colonyGrid.getHeight());
-
-        Grid.PathNode currNode = new Grid.PathNode(node);
-        currNode.parentNode = null;
-        currNode.G = 0;
-        currNode.H = Math.abs(targetNode.getX() - currNode.x) + Math.abs(targetNode.getY() - currNode.y)*10;
-        visitedMap.put(currNode.node, currNode.node);
-
-        while(currNode != null){
-            if(currNode.node == targetNode){
-                found = true;
-                target = currNode;
-                currNode = null;
-                break;
-            }
-
-            visitedMap.put(currNode.node, currNode.node);
-            Grid.Node[] neighbors = this.blackBoard.colonyGrid.getNeighbors8(currNode.node);
-
-            //Add all the (valid) neighbors to the openList.
-            for (Grid.Node neighbor : neighbors) {
-                if (neighbor == null || visitedMap.containsKey(neighbor) || neighbor.getTerrainTile() == null) continue;
-                visitedMap.put(neighbor, neighbor);
-
-                //Add the neighbor node to the visited map and open list.
-                Grid.PathNode neighborNode = new Grid.PathNode(neighbor);
-
-                //Set this neighbors values and add it to the openList.
-                int[] dir = this.blackBoard.colonyGrid.getDirection(neighborNode.node, currNode.node);
-                if (dir[0] == 0 || dir[1] == 0) //This is straight, not diagonal.
-                    neighborNode.G = currNode.G + 10;
-                else
-                    neighborNode.G = currNode.G + 14; //Diagonal.
-
-                //Get the TerrainTile and check if the tile is to be avoided.
-                Grid.TerrainTile tile = ColonyGame.worldGrid.getNode(neighborNode.x, neighborNode.y).getTerrainTile();
-                if (neighborNode.hasEnts() || (tile != null && tile.tileRef.avoid))
-                    neighborNode.B = 500;
-
-                //Set the H value, add it to the openList, and make its parent the current Node.
-                neighborNode.H = Math.abs(targetNode.getX() - currNode.x) + Math.abs(targetNode.getY() - currNode.y) * 10;
-
-                openList.add(neighborNode);
-                neighborNode.parentNode = currNode;
-            }
-
-            currNode = openList.poll();
-        }
-
-        float squareSize = this.blackBoard.colonyGrid.getSquareSize();
-        //If a path was found, record the path.
-        if(found) {
-            currNode = target;
-            while (currNode.parentNode != null) {
-                path.add(new Vector2(currNode.x*squareSize + squareSize*0.5f, currNode.y*squareSize + squareSize*0.5f));
-                currNode = currNode.parentNode;
-            }
-        }
-
-        openList.clear();
-        visitedMap.clear();
-        return path;
     }
 }
--- core/src/com/mygdx/game/component/Enterable.java
+++ core/src/com/mygdx/game/component/Enterable.java
@@ -1,15 +1,17 @@
 package com.mygdx.game.component;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by Paha on 7/17/2015.
  */
 public class Enterable extends Component{
-    private Vector2[] originalEnterPositions;
-    private Vector2[] enterPositions;
+    private Vector2[] originalEnterPositions; //The original enter positions offsets.
+    private Vector2[] enterPositions; //The updated positions including rotation and world location of the Entity owner.
 
     private float lastRotation = 0;
+    private Vector2 lastPosition = new Vector2(0,0);
 
     public Enterable() {
         super();
@@ -30,22 +32,45 @@ public class Enterable extends Component{
         super.update(delta);
     }
 
+    /**
+     * Sets the original enter positions which are the offsets from the center of the Entity that is Enterable.
+     * @param positions The offset positions from the center of the Entity.
+     */
     public void setEnterPositions(Vector2... positions){
-        this.originalEnterPositions = positions;
-        for(int i=0;i<positions.length;i++)
-            this.enterPositions[i] = new Vector2(positions[i].x, positions[i].y); //Copy a separate vector2;
+        this.originalEnterPositions = positions; //Assign to the original.
+        this.enterPositions = new Vector2[positions.length]; //Make a new array.
+        this.calculatePositions(this.originalEnterPositions);
     }
 
+    /**
+     * Gets the world enter positions of this Entity, which are recalculated if rotation or position has changed.
+     * @return An array of world positions.
+     */
     public Vector2[] getEnterPositions(){
         Transform trans = this.owner.getTransform(); //Cache the transform
-        if(this.lastRotation != trans.getRotation()){ //If the rotations are not the same
-            //For each position in the original positions
-            for(int i=0;i<this.originalEnterPositions.length;i++){
-                Vector2 pos = this.originalEnterPositions[i];
-                //this.enterPositions
-
-            }
+        //If the rotations/positions are not the same, calculate the new positions and update the last rotation/position.
+        if(this.lastRotation != trans.getRotation() || this.lastPosition.x != trans.getPosition().x || this.lastPosition.y != trans.getPosition().y){
+            this.calculatePositions(this.originalEnterPositions);
             this.lastRotation = trans.getRotation();
+            this.lastPosition.set(trans.getPosition().x, trans.getPosition().y);
+        }
+
+        return this.enterPositions;
+    }
+
+    /**
+     * Calculates the enterPositions using the 'original' positions parameter.
+     * @param original The original positions to use for calculations.
+     * @return The changed enter positions.
+     */
+    private Vector2[] calculatePositions(Vector2[] original){
+        Transform trans = this.owner.getTransform();
+        //Copy the positions and factor in rotation and Entity owner position
+        for(int i=0;i<original.length;i++) {
+            Vector2 pos = this.originalEnterPositions[i];
+            float x = MathUtils.cos(trans.getRotation())*pos.x;
+            float y = MathUtils.sin(trans.getRotation())*pos.y;
+            this.enterPositions[i] = new Vector2(trans.getPosition().x + x, trans.getPosition().y + y);
         }
 
         return this.enterPositions;
